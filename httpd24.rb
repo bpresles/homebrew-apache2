b9e18d08e5b272691dd227dc2cacad34be80eeff
bpresles <bpresles@gmail.com>
2014-06-12 10:25:04 +0200

    Updated apr version

--- httpd24.rb
+++ httpd24.rb
@@ -14,7 +14,7 @@ homepage 'https://httpd.apache.org/'
   # Apache 2.4 no longer bundles apr or apr-util so we have to fetch
   # it manually for each build
   def fetch_apr
-    ["apr-1.5.0", "apr-util-1.5.3"].each do |tb|
+    ["apr-1.5.1", "apr-util-1.5.3"].each do |tb|
       curl "-s", "-o", "#{tb}.tar.gz", "https://www.apache.org/dist/apr/#{tb}.tar.gz"
       system "tar -xzf #{tb}.tar.gz"
       dir = tb.rpartition('-')[0]
